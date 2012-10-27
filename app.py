0394debee72d363d6e98332ae1df09d1bd2fc606
gdubya <gareth@garethwestern.com>
2012-10-28 00:01:48 +0200

    added link to imdb

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import os
 from flask import Flask
+from flask import render_template
+import imdb
+import random
 
 app = Flask(__name__)
+ia = imdb.IMDb()
+
+app.debug=True
+
+def selectRandomMovie():
+    movies = ia.search_movie('% age%', results=500)
+    movie = movies[random.randint(0, len(movies))]
+    return movie
 
 @app.route('/')
 def hello():
-    return 'Hello World!!!!'
+    movie = selectRandomMovie()
+    while not 'Age' in movie['title']:
+      movie = selectRandomMovie()
+    title = movie['title'].replace('Age','Cage')
+    return render_template('age.html',title=title,id=movie.getID())
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
